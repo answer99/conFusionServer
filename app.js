c190dcc804243a7ceef92db23a8bf459956bd22f
Vincent Chao <vincentchao99@gmail.com>
2019-06-09 17:15:14 +0900

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var logger = require('morgan');
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,7 +18,7 @@ var leaderRouter = require('./routes/leaderRouter');
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/cinFusion';
+const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -41,25 +43,21 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log('Get req.session: ', req.session);
-  if(!req.session.user){
+  console.log(req.user);
+  if(!req.user){
     var err = new Error('You are not authenticated!'); 
     err.status = 403;
     return next(err);
   }
   else{
-    if(req.session.user === 'authenticated'){
-      next();
-    }
-    else{
-      var err = new Error('You are not authenticated!');
-      err.status = 401;
-      next(err);
-    }
+    next();
   }
 }
 
