801c9cf65ae2559b6c5623d4a53d8947b4e962ec
Vincent Chao <vincentchao99@gmail.com>
2019-06-23 18:36:41 +0900

    Assignment 4: access cotrol

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,14 @@ var passport = require('passport');
 var authenticate = require('../authenticate');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  User.find({})
+    .then((users) => {
+        res.StatusCode = 200;
+        res.setHeader('Content-type', 'application/json');
+        res.json(users)
+    }, (err) => next(err))
+    .catch(err => next(err));
 });
 
 router.post('/signup', (req, res, next) => {
